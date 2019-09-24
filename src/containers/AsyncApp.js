233eb9f22cdb1c597e044c0bf7f264abbdb7c148
GriffinJ <jrgriffiniii@gmail.com>
2019-09-23 22:06:24 -0400

    Adding Material UI

--- src/containers/AsyncApp.js
+++ src/containers/AsyncApp.js
@@ -4,6 +4,23 @@ import { connect } from 'react-redux'
 import { updateWorks } from '../actions'
 import { Map, TileLayer, GeoJSON } from 'react-leaflet'
 import GeoMap from './GeoMap';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
 
 class AsyncApp extends Component {
   constructor(props) {
@@ -29,22 +46,29 @@ class AsyncApp extends Component {
 
     return (
       <div>
+        <Container maxWidth="lg">
+          <Typography variant="h1" component="h1" gutterBottom align="center" style={this.props.style.h1}>Linked Geo Data 🌐</Typography>
+        </Container>
+
         <GeoMap works={ this.props.works }/>
 
-        <div>
-          {lastUpdated && (
-            <span>
-              Last updated at {new Date(lastUpdated).toLocaleTimeString()}.{' '}
-            </span>
-          )}
-        </div>
+        <Grid container spacing={2} align="center" gutterBottom style={this.props.style.grid}>
+          <Grid item xs={6}>
+            <Card>
+              <CardContent>
+                {lastUpdated && (
+                  <span>Last updated at {new Date(lastUpdated).toLocaleTimeString()}.{' '}</span>
+                )}
+              </CardContent>
+              <CardActions align="center">
+                <Button variant="contained" onClick={this.handleRefreshClick} disabled={ isRequesting } >Refresh</Button>
+              </CardActions>
+            </Card>
+          </Grid>
 
-        <div>
-          {!isRequesting && (
-            <button onClick={this.handleRefreshClick}>Refresh</button>
-          )}
-        </div>
-
+          <Grid item xs={6}>
+          </Grid>
+        </Grid>
       </div>
     )
   }
@@ -54,7 +78,19 @@ AsyncApp.propTypes = {
   works: PropTypes.array.isRequired,
   isRequesting: PropTypes.bool.isRequired,
   lastUpdated: PropTypes.number,
-  dispatch: PropTypes.func.isRequired
+  dispatch: PropTypes.func.isRequired,
+  style: PropTypes.object
+}
+
+AsyncApp.defaultProps = {
+  style: {
+    h1: {
+      marginTop: "0.35em"
+    },
+    grid: {
+      marginTop: "0.35em"
+    }
+  }
 }
 
 function mapStateToProps(state) {
